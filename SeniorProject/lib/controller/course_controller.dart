0c470b1bc9b1b664bc00bbc30409f3ca0064237b
alvaroprado00 <72304152+alvaroprado00@users.noreply.github.com>
2022-02-10 13:15:39 -0500

    Adding some comments

--- SeniorProject/lib/controller/course_controller.dart
+++ SeniorProject/lib/controller/course_controller.dart
@@ -1,15 +1,22 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cyber/config/k_collection_names_firebase.dart';
 import 'package:cyber/model/course.dart';
 
 class CourseController{
 
+  /**
+   * If you create a reference to the collection this way, then automatically the data in
+   * the snapshot is in the form of a Course object, you dont have to do any changes
+   * after getting the snapshot
+   */
   final courseRef = FirebaseFirestore.instance.collection(courseCollectionName).withConverter<Course>(
     fromFirestore: (snapshot, _) => Course.fromJson(snapshot.data()!),
     toFirestore: (course, _) => course.toJson(),
   );
 
+  /**
+   * Function to get a Course from the database when the title is specified
+   */
   Future getCourse({required String title}) async {
 
     return await courseRef
