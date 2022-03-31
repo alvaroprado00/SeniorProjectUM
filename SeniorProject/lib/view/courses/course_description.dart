ed65d3a531a28a5a46895d07e201a8326504c7e3
beltranmoreno <beltranmoreno00@gmail.com>
2022-03-31 17:06:33 -0400

    merging alvaros commits

--- SeniorProject/lib/view/courses/course_description.dart
+++ SeniorProject/lib/view/courses/course_description.dart
@@ -29,45 +29,42 @@ class CourseDescription extends GetView<ActiveUserController> {
 
     // Before building the page I need to get the new-course from the DB
 
- return FutureBuilder(
-        future: courseController.getCourseByID(id: courseID),
-        builder: (BuildContext context, AsyncSnapshot snapshot){
-          if (snapshot.hasData) {
+    return FutureBuilder(
+      future: courseController.getCourseByID(id: courseID),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          //Once I get the new-course I assign its value to a global variable
+          // so I can access the info from other pages easily
+          globals.activeCourse = snapshot.data;
+          globals.activeQuestionNum = 1;
+          globals.userProgress = [];
 
-              //Once I get the new-course I assign its value to a global variable
-              // so I can access the info from other pages easily
-              globals.activeCourse = snapshot.data;
-              globals.activeQuestionNum = 1;
-              globals.userProgress = [];
-
-              if (controller.isCurrentCourse(courseID: courseID)) {
-                globals.activeQuestionNum =
-                    globals.activeUser!.currentCourse!.progress.length + 1;
-                globals.userProgress =
-                    globals.activeUser!.currentCourse!.progress;
-              }
-
-
-              return Obx(() => CourseDescriptionContent(
-                comesFromFeaturePage: false,
-                  course: snapshot.data,
-                  isSaved: controller.isSaved(courseID: courseID),
-                  isCurrentCourse:
-                      controller.isCurrentCourse(courseID: courseID),
-                  isCompleted: controller.isCompleted(courseID: courseID)));
-          } else if (snapshot.hasError) {
-            return Scaffold(
-              body: Center(
-                child: Text('Error: ${snapshot.error}', style: getHeadingStyleBlue(),),
-              ),
-            );
-          }else{
-            return Scaffold(body: Center(child: CircularProgressIndicator()));
+          if (controller.isCurrentCourse(courseID: courseID)) {
+            globals.activeQuestionNum =
+                globals.activeUser!.currentCourse!.progress.length + 1;
+            globals.userProgress = globals.activeUser!.currentCourse!.progress;
           }
-        },
- );
 
-
+          return Obx(() => CourseDescriptionContent(
+              comesFromFeaturePage: false,
+              course: snapshot.data,
+              isSaved: controller.isSaved(courseID: courseID),
+              isCurrentCourse: controller.isCurrentCourse(courseID: courseID),
+              isCompleted: controller.isCompleted(courseID: courseID)));
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Text(
+                'Error: ${snapshot.error}',
+                style: getHeadingStyleBlue(),
+              ),
+            ),
+          );
+        } else {
+          return Scaffold(body: Center(child: CircularProgressIndicator()));
+        }
+      },
+    );
   }
 }
 
@@ -76,16 +73,16 @@ class CourseDescription extends GetView<ActiveUserController> {
  * When built, it shows the info from the new-course
  */
 class CourseDescriptionContent extends StatelessWidget {
-
   const CourseDescriptionContent(
       {required Course this.course,
       required bool this.isSaved,
       required this.isCurrentCourse,
-        required this.comesFromFeaturePage,
+      required this.comesFromFeaturePage,
       required this.isCompleted});
 
   final Course course;
-  final bool comesFromFeaturePage; //This bool is needed to not show the back button
+  final bool
+      comesFromFeaturePage; //This bool is needed to not show the back button
   final bool isSaved;
   final bool isCurrentCourse;
   final bool isCompleted;
@@ -100,8 +97,8 @@ class CourseDescriptionContent extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
-      floatingActionButton: course.isFeatured!? FeaturedButton(): null,
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerTop,
+      floatingActionButton: course.isFeatured! ? FeaturedButton() : null,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerTop,
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -119,12 +116,14 @@ class CourseDescriptionContent extends StatelessWidget {
                     ),
                   ),
                 ),
-                this.comesFromFeaturePage?
-              SizedBox(height: 0,):
-              Positioned(
-                top: 0.08 * heightOfScreen,
-                child: getBackButton(context: context),
-              ),
+                this.comesFromFeaturePage
+                    ? SizedBox(
+                        height: 0,
+                      )
+                    : Positioned(
+                        top: 0.08 * heightOfScreen,
+                        child: getBackButton(context: context),
+                      ),
               ],
             ),
             Padding(
@@ -154,14 +153,15 @@ class CourseDescriptionContent extends StatelessWidget {
                           ],
                         ),
                       ]),
-
                   Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Text(
-                       course.isFeatured!?'${course.experiencePoints} XP x2':'${course.experiencePoints} XP',
-                      style: getNormalTextStyleYellow(),
-                    ),
+                        course.isFeatured!
+                            ? '${course.experiencePoints} XP x2'
+                            : '${course.experiencePoints} XP',
+                        style: getNormalTextStyleYellow(),
+                      ),
                       SizedBox(
                         width: 0.05 * widthOfScreen,
                       ),
@@ -229,7 +229,7 @@ class CourseDescriptionContent extends StatelessWidget {
           ],
         ),
       ),
-    ));
+    );
   }
 }
 
@@ -268,7 +268,7 @@ getContentForOutcomes({required List<String> outcomes}) {
 }
 
 /**
- * Method to get Container Showing two lines of text. Used for both 
+ * Method to get Container Showing two lines of text. Used for both
  * showing the content for the last completed new-course and the progress in the
  * current new-course
  */
@@ -346,17 +346,27 @@ class FeaturedButton extends StatelessWidget {
             barrierDismissible: true,
             builder: (BuildContext context) {
               return AlertDialog(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-               // title: Text('Featured!', style: getSubheadingStyleYellow(), textAlign: TextAlign.center,),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                // title: Text('Featured!', style: getSubheadingStyleYellow(), textAlign: TextAlign.center,),
 
-                content: Image.asset('assets/images/doubleXP.png', width: 0.25*widthOfScreen, height: 0.3*heightOfScreen,),
+                content: Image.asset(
+                  'assets/images/doubleXP.png',
+                  width: 0.25 * widthOfScreen,
+                  height: 0.3 * heightOfScreen,
+                ),
               );
             });
       },
-      icon: const Icon(Icons.bolt, color: tertiaryColor,),
-      label: Text('Featured', style: getSubheadingStyleWhite(),),
+      icon: const Icon(
+        Icons.bolt,
+        color: tertiaryColor,
+      ),
+      label: Text(
+        'Featured',
+        style: getSubheadingStyleWhite(),
+      ),
       backgroundColor: secondaryColor,
     );
   }
--- SeniorProject/lib/view/page_view.dart
+++ SeniorProject/lib/view/page_view.dart
@@ -11,10 +11,8 @@ import 'package:get/get.dart';
 
 import '../controller/active_user_controller.dart';
 import '../controller/user_controller.dart';
-import '../globals.dart';
 import '../model/user_custom.dart';
 import 'admin/new-course/new_course_page.dart';
-
 
 class PageViewScreen extends StatelessWidget {
   const PageViewScreen({Key? key}) : super(key: key);
@@ -26,17 +24,15 @@ class PageViewScreen extends StatelessWidget {
       future: initializeUser(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         if (snapshot.hasData) {
-
-
           return PageViewScreenContent();
         } else if (snapshot.hasError) {
           return Scaffold(
               body: Center(
-                child: Text(
-                  'Error: ${snapshot.error}',
-                  style: getNormalTextStyleBlue(),
-                ),
-              ));
+            child: Text(
+              'Error: ${snapshot.error}',
+              style: getNormalTextStyleBlue(),
+            ),
+          ));
         } else {
           return Scaffold(body: Center(child: CircularProgressIndicator()));
         }
@@ -46,8 +42,7 @@ class PageViewScreen extends StatelessWidget {
 }
 
 class PageViewScreenContent extends StatefulWidget {
-  const PageViewScreenContent({Key? key})
-      : super(key: key);
+  const PageViewScreenContent({Key? key}) : super(key: key);
 
   @override
   _PageViewScreenContentState createState() => _PageViewScreenContentState();
@@ -57,7 +52,6 @@ class _PageViewScreenContentState extends State<PageViewScreenContent> {
   PageController pageController = PageController(initialPage: 0);
 
   int _selectedIndex = 0;
-
 
   void onTapped(int index) {
     setState(() {
@@ -69,8 +63,7 @@ class _PageViewScreenContentState extends State<PageViewScreenContent> {
 
   @override
   Widget build(BuildContext context) {
-
-    ActiveUserController activeUserController=Get.find();
+    ActiveUserController activeUserController = Get.find();
     return Scaffold(
       body: PageView(
         controller: pageController,
@@ -116,11 +109,12 @@ class _PageViewScreenContentState extends State<PageViewScreenContent> {
                 decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     border: Border.all(color: primaryColor, width: 0.5)),
-                child: Obx(()=>CircleAvatar(
-                  backgroundImage: NetworkImage('https://robohash.org/${activeUserController.profilePictureActive}'),
-                  backgroundColor: Colors.transparent,
-                  radius: 12,
-                )),
+                child: Obx(() => CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://robohash.org/${activeUserController.profilePictureActive}'),
+                      backgroundColor: Colors.transparent,
+                      radius: 12,
+                    )),
               ),
               label: 'Profile')
         ],
@@ -128,15 +122,15 @@ class _PageViewScreenContentState extends State<PageViewScreenContent> {
     );
   }
 }
-Future initializeUser() async {
 
-  try{
+Future initializeUser() async {
+  try {
     //I get the active user
-    UserCustom uc=await UserController.getActiveUser();
+    UserCustom uc = await UserController.getActiveUser();
 
     //I initialize the global variable activeUser that is used by the GetX
     //controller to initialize the info
-    activeUser=uc;
+    activeUser = uc;
 
     if (Get.isRegistered<ActiveUserController>()) {
       Get.delete<ActiveUserController>();
@@ -144,9 +138,7 @@ Future initializeUser() async {
     Get.put(ActiveUserController());
 
     return Future.value('Done');
-
-  }catch(error){
+  } catch (error) {
     throw Exception('Error initializing the user');
   }
 }
-
